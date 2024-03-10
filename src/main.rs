dce34728472655441ac3c93fc49c08cd0bccb626
Doruk Cem <doruk.cem@gmail.com>
2024-03-10 17:12:13 +0300

    Item as structure

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,18 @@
+use std::fmt;
 use std::io::{self, Write};
+
+struct Item {
+    name: String,
+    done: bool,
+}
+impl fmt::Display for Item {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{} [{}]", self.name, if self.done { 'X' } else { ' ' })
+    }
+}
+
 fn main() {
-    let mut todos: Vec<String> = Vec::new();
+    let mut todos: Vec<Item> = Vec::new();
 
     loop {
         let mut input = String::new();
@@ -12,19 +24,37 @@ fn main() {
         let tokens: Vec<&str> = input.trim().split_whitespace().collect();
 
         match tokens[..] {
-            ["add", item] => {
-                let item = String::from(item);
-                println!("Added item: {item}");
+            ["add", item_name] => {
+                let item_name = String::from(item_name);
+                println!("Added item: {item_name}");
+                let item = Item {
+                    name: item_name,
+                    done: false,
+                };
                 todos.push(item);
             }
-            ["remove", item] => {
-                let item = String::from(item);
-                if let Some(index) = todos.iter().position(|x| *x == item) {
+
+            ["remove", item_name] => {
+                let item_name = String::from(item_name);
+                if let Some(index) = todos.iter().position(|x| (*x).name == item_name) {
                     todos.remove(index);
                 }
-                println!("Removed item: {item}");
+                println!("Removed item: {item_name}");
             }
-            ["show"] => println!("{:?}", todos),
+
+            ["toggle", item_name] => {
+                let item_name = String::from(item_name);
+                if let Some(index) = todos.iter().position(|x| (*x).name == item_name) {
+                    todos[index].done = !todos[index].done
+                }
+            }
+
+            ["show"] => {
+                for item in todos.iter() {
+                    println!("   {item}");
+                }
+            }
+
             ["exit"] => {
                 println!("Exited applciation");
                 break;
