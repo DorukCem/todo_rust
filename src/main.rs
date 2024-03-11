eb8181c872d16bd8887b4690719e8a4efd9656ee
Doruk Cem <doruk.cem@gmail.com>
2024-03-11 17:28:23 +0300

    refactor

--- src/main.rs
+++ src/main.rs
@@ -21,45 +21,57 @@ fn main() {
         io::stdin()
             .read_line(&mut input)
             .expect("Failed to read line");
-        let tokens: Vec<&str> = input.trim().split_whitespace().collect();
+        let tokens: Vec<&str> = input.split_whitespace().collect();
 
         match tokens[..] {
             ["add", item_name] => {
-                let item_name = String::from(item_name);
-                println!("Added item: {item_name}");
                 let item = Item {
-                    name: item_name,
+                    name: String::from(item_name),
                     done: false,
                 };
                 todos.push(item);
+                println!("Added item: {item_name}");
             }
 
             ["remove", item_name] => {
-                let item_name = String::from(item_name);
-                if let Some(index) = todos.iter().position(|x| (*x).name == item_name) {
+                if let Some(index) = todos.iter().position(|x| x.name == item_name) {
                     todos.remove(index);
+                    println!("Removed item: {item_name}");
+                } else {
+                    eprintln!("Could not find item: {item_name}");
                 }
-                println!("Removed item: {item_name}");
             }
 
             ["toggle", item_name] => {
-                let item_name = String::from(item_name);
-                if let Some(index) = todos.iter().position(|x| (*x).name == item_name) {
-                    todos[index].done = !todos[index].done
+                if let Some(index) = todos.iter().position(|x| x.name == item_name) {
+                    todos[index].done = !todos[index].done;
+                    println!("Toggled item: {item_name}");
+                } else {
+                    eprintln!("Could not find item: {item_name}");
                 }
             }
 
             ["show"] => {
-                for item in todos.iter() {
+                for item in &todos {
                     println!("   {item}");
                 }
+            }
+
+            ["help"] => {
+                println!("Available commands:");
+                println!("  add <item_name>   - Add a new item to the list");
+                println!("  remove <item_name>- Remove an item from the list");
+                println!("  toggle <item_name>- Toggle the status of an item (done/undone)");
+                println!("  show              - Show all items in the list");
+                println!("  help              - Show this help message");
+                println!("  exit              - Exit the application");
             }
 
             ["exit"] => {
                 println!("Exited applciation");
                 break;
             }
-            _ => println!("invalid input"),
+            _ => eprintln!("invalid input"),
         }
     }
 }
